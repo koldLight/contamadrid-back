16f741e5d7724fef3df7446181ec96b0ac12573b
Luz Frias <luzfrias@gmail.com>
2016-08-10 20:54:36 +0200

    Added utils to query the DB and return the rows as JSON

--- server.py
+++ server.py
@@ -1,25 +1,63 @@
 import bottle
 import bottle_pgsql
 import json
+import decimal
 
-app = bottle.default_app()
+###############################################################################
+# Initialization                                                              #
+###############################################################################
+
+app = application = bottle.Bottle()
+app.debug = True
 app.config.load_config("conf/conf.ini")
 
-plugin = bottle_pgsql.Plugin("dbname="    + app.config["db.database"] +
-                             " user="     + app.config["db.user"] +
+plugin = bottle_pgsql.Plugin("dbname=" + app.config["db.database"] +
+                             " user=" + app.config["db.user"] +
                              " password=" + app.config["db.pass"] +
-                             " host="     + app.config["db.host"])
+                             " host=" + app.config["db.host"])
 app.install(plugin)
+
+###############################################################################
+# Utils                                                                       #
+###############################################################################
+
+
+class number_str(float):
+
+  def __init__(self, o):
+    self.o = o
+
+  def __repr__(self):
+    return str(self.o)
+
+
+def decimal_serializer(o):
+  if isinstance(o, decimal.Decimal):
+    return number_str(o)
+  raise TypeError(repr(o) + " is not JSON serializable")
+
+
+def query_db(db, query, args=(), one=False):
+  db.execute(query, args)
+  return db.fetchall()
+
+
+def to_json(rows):
+  return json.dumps(rows, default=decimal_serializer)
+
+###############################################################################
+# Controllers                                                                 #
+###############################################################################
+
 
 @app.route("/stations")
 def stations(db):
   rows = query_db(db, "SELECT * from station")
-  return json.dumps(rows)
+  return to_json(rows)
 
-def query_db(db, query, args=(), one=False):
-    db.execute(query, args)
-    r = [dict((db.description[i][0], value) \
-               for i, value in enumerate(row)) for row in db.fetchall()]
-    return (r[0] if r else None) if one else r
+###############################################################################
+# Launch the server                                                           #
+###############################################################################
 
-app.run(host="localhost", port=8080, debug=True)
+if __name__ == "__main__":
+  app.run(host='0.0.0.0', port=8080, debug=True)
