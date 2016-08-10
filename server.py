e939116a22b11afbbc6c1c985236ae604518a2fb
Luz Frias <luzfrias@gmail.com>
2016-08-10 22:15:37 +0200

    Added CORS support

--- server.py
+++ server.py
@@ -1,5 +1,5 @@
 import bottle
-from bottle import request
+from bottle import request, response
 import bottle_pgsql
 import json
 import decimal
@@ -22,6 +22,26 @@ app.install(plugin)
 ###############################################################################
 # Utils                                                                       #
 ###############################################################################
+
+
+class EnableCors(object):
+  name = 'enable_cors'
+  api = 2
+
+  def apply(self, fn, context):
+    def _enable_cors(*args, **kwargs):
+      # set CORS headers
+      response.headers['Access-Control-Allow-Origin'] = 'contamadrid.es'
+      response.headers[
+          'Access-Control-Allow-Methods'] = 'GET, POST, PUT, OPTIONS'
+      response.headers[
+          'Access-Control-Allow-Headers'] = 'Origin, Accept, Content-Type, X-Requested-With, X-CSRF-Token'
+
+      if bottle.request.method != 'OPTIONS':
+        # actual request; reply with the actual response
+        return fn(*args, **kwargs)
+
+    return _enable_cors
 
 
 def date_serializer(o):
@@ -70,6 +90,8 @@ def measures(db):
 ###############################################################################
 # Launch the server                                                           #
 ###############################################################################
+
+app.install(EnableCors())
 
 if __name__ == "__main__":
   app.run(host='0.0.0.0', port=8080, debug=True)
